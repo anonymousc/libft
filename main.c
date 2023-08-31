27e824e404af9a72167c183259b1ac8b332768f9
anonymousc <dodmin15@gmail.com>
2023-08-31 20:15:29 +0100

    ctype are finished

--- main.c
+++ main.c
@@ -13,8 +13,9 @@
     write(1,&a,1);
 }
 */
+/*
 int main (){
-    char str[] = "123adam";
+    char str[] = "";
     int alpha = 0, num = 0,i;
     for(i = 0;str[i] != '\0'; i++)
     {
@@ -29,3 +30,16 @@ int main (){
     }
     printf("alpahbets are : %d\n numbers are : %d\n",alpha,num);
 }
+*/
+/*
+int main()
+{
+    int i = 65;
+    int b = 65;
+    //printf("%d\n",isalpha(b));
+    int a = isprint(b);
+    printf("%d",a);
+    write(1,"\n",1);
+    write(1,&a,1);
+}
+*/
