56114469e322b582673024097c2da7e9c3585ba3
Amine Essadiki <aessadik@e2r8p4.1337.ma>
2023-11-19 01:23:29 +0100

    done

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: aessadik <aessadik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 10:25:23 by aessadik          #+#    #+#             */
-/*   Updated: 2023/11/14 00:59:55 by aessadik         ###   ########.fr       */
+/*   Updated: 2023/11/19 01:23:06 by aessadik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
 	size_t			i;
+	unsigned char	*tmp1;
+	unsigned char	*tmp2;
+	int				diff;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
+	diff = 0;
 	i = 0;
+	tmp1 = (unsigned char *)s1;
+	tmp2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if ((str1[i] - str2[i]) != 0)
-			break ;
+		diff = tmp1[i] - tmp2[i];
+		if (tmp1[i] != tmp2[i])
+			return (diff);
 		i++;
 	}
-	return (*((unsigned char *)str1 + i) - *((unsigned char *)str2 + i));
+	return (0);
 }
