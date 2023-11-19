b9e4f0ff967ab6679fa500acf30b2ada835572d7
Amine Essadiki <aessadik@e2r8p4.1337.ma>
2023-11-19 01:12:02 +0100

    done

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,56 @@
 /*   By: aessadik <aessadik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:32:41 by aessadik          #+#    #+#             */
-/*   Updated: 2023/11/12 20:37:42 by aessadik         ###   ########.fr       */
+/*   Updated: 2023/11/19 01:05:52 by aessadik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	len_word(char const *s, char del)
+{
+	int	index;
+	int	count;
+
+	index = 0;
+	count = 0;
+	if (!s)
+		return (0);
+	while (s[index] != '\0')
+	{
+		while (s[index] == del)
+			index++;
+		if (s[index] != '\0')
+			count++;
+		while (s[index] != del && s[index] != '\0')
+			index++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		index;
+	int		size;
+	int		index2;
+	int		lenght;
+	char	**str;
+
+	lenght = len_word(s, c);
+	index = 0;
+	index2 = 0;
+	str = (char **)malloc((len_word(s, c) + 1) * sizeof(char *));
+	if (!str)
+		return (NULL);
+	while (index2 < lenght)
+	{
+		while (s[index] == c && s[index] != '\0')
+			index++;
+		size = index;
+		while (s[index] != c && s[index] != '\0')
+			index++;
+		str[index2++] = ft_substr(s, size, index - size);
+	}
+	str[index2] = NULL;
+	return (str);
 }
